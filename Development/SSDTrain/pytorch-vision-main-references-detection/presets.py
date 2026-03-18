7aa35e3edfc082e96307193afa121ed7c29c5433
LukasKratochvila <kratochvila.lukas@gmail.com>
2026-03-18 16:20:40 +0100

    Update presets.py
    Add albumetantation transforms

--- Development/SSDTrain/pytorch-vision-main-references-detection/presets.py
+++ Development/SSDTrain/pytorch-vision-main-references-detection/presets.py
@@ -2,7 +2,8 @@ from collections import defaultdict
 
 import torch
 import transforms as reference_transforms
-
+import albumentations as A
+import numpy as np
 
 def get_modules(use_v2):
     # We need a protected import to avoid the V2 warning in case just V1 is used
@@ -28,6 +29,7 @@ class DetectionPresetTrain:
         use_v2=False,
     ):
 
+        self.aug=data_augmentation
         T, tv_tensors = get_modules(use_v2)
 
         transforms = []
@@ -56,6 +58,7 @@ class DetectionPresetTrain:
         elif data_augmentation == "ssd":
             fill = defaultdict(lambda: mean, {tv_tensors.Mask: 0}) if use_v2 else list(mean)
             transforms += [
+                T.Resize((300,300)),
                 T.RandomPhotometricDistort(),
                 T.RandomZoomOut(fill=fill),
                 T.RandomIoUCrop(),
@@ -66,8 +69,35 @@ class DetectionPresetTrain:
                 T.RandomIoUCrop(),
                 T.RandomHorizontalFlip(p=hflip_prob),
             ]
-        else:
-            raise ValueError(f'Unknown data augmentation policy "{data_augmentation}"')
+        elif data_augmentation == "alb":
+            alb_transforms = [
+                #A.Blur(p=0.2),
+                #A.MedianBlur(p=0.2),
+                #A.ToGray(p=0.2),
+                #A.CLAHE(p=0.2),
+                #A.RandomBrightnessContrast(0.2),
+                #A.RandomGamma(p=0.2),
+                #A.ImageCompression(quality_range=(75,100), p=0.2),
+                A.Resize(height=300, width=300),
+                
+                A.PhotoMetricDistort(p=0.2),
+                A.RandomScale(scale_limit=(-0.9,0)),
+                A.BBoxSafeRandomCrop(p=0.2),
+                A.HorizontalFlip(p=0.5),                
+                
+                A.AutoContrast(p=0.2),
+                A.Illumination(p=0.2),
+                A.MotionBlur(p=0.2),
+                A.Defocus(p=0.2),
+                A.ChromaticAberration(p=0.2),
+                A.ISONoise(p=0.2),
+                A.pytorch.transforms.ToTensorV2()
+            ]
+            self.transforms = A.Compose(alb_transforms, bbox_params=A.BboxParams("pascal_voc"#'coco', # Specify input format label_fields=['class_labels'] # Specify label argument name(s)
+            ))
+            return
+        #else:
+        #    raise ValueError(f'Unknown data augmentation policy "{data_augmentation}"')
 
         if backend == "pil":
             # Note: we could just convert to pure tensors even in v2.
@@ -84,8 +114,22 @@ class DetectionPresetTrain:
 
         self.transforms = T.Compose(transforms)
 
-    def __call__(self, img, target):
-        return self.transforms(img, target)
+    def __call__(self, img, tgr):
+        if self.aug == "alb":
+            b=tgr["boxes"].numpy()
+            #print(b, img.size)
+            #if b[0,0]+b[0,2]>img.size[0]:
+            #    b[0,2] = img.size[0]-b[0,0]-2
+            #if b[0,1]+b[0,3]>img.size[1]:
+            #    b[0,3] = img.size[1]-b[0,1]-2
+            #print(b)
+            transformed = self.transforms(image=np.array(img), bboxes=b)
+            #print(transformed["bboxes"].astype(np.uint8))
+            image = transformed["image"]
+            target = tgr
+            target["boxes"] = torch.from_numpy(transformed["bboxes"])
+            return reference_transforms.ToDtype(torch.float, scale=True)(image, target)
+        return self.transforms(img, tgr)
 
 
 class DetectionPresetEval:
